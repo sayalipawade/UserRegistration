66afc468dd298c44dcdec362d0772a41ef2b4781
sayalipawade <sayalipawade05@gmail.com>
2020-04-02 17:09:27 +0530

    Added:Clear all email samples

--- src/main/java/com/user/User.java
+++ src/main/java/com/user/User.java
@@ -6,7 +6,7 @@ public class User
 {
     //constants
     public static String NAMEPATTERN="^[A-Z][a-zA-Z]{2,}$";
-    public static String EMAILPATTERN="^[a-zA-Z0-9]{3,}+[@][a-zA-Z0-9]{3,}+[.][a-zA-Z]{3,}$";
+    public static String EMAILPATTERN="^[a-zA-Z0-9[-+]]{1,}([.]?[a-zA-Z0-9]{1,})?[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-z]{2,3}([.]?[a-z]{2,3})?$";
     public static String MOBILEPATTERN="^[0-9]{2}[0-9]{10}";
     public static String PASSWORDPATTERN="^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[0-9a-z])(?=.*[A-Z])(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$";
     //Pattern checking method
@@ -52,7 +52,7 @@ public class User
         mobileNo=sc.next();
         patternChecking(mobileNo,MOBILEPATTERN);
 
-        //Password should contain at least one uppercase character
+        //Checking password pattern
         System.out.println("Enter Password");
         password=sc.next();
         patternChecking(password,PASSWORDPATTERN);
