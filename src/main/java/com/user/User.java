3e380a0cd299b869c6caa7b29bee01582bce8556
sayalipawade <sayalipawade05@gmail.com>
2020-04-02 12:47:00 +0530

    Added:Checking first name is valid or not

--- src/main/java/com/user/User.java
+++ src/main/java/com/user/User.java
@@ -1,8 +1,32 @@
 package com.user;
+
+import java.util.Scanner;
+
 public class User
 {
+    //constants
+    public static String NAMEPATTERN="^[A-Z][a-zA-Z]{2,}$";
+    //Pattern checking method
+    public static void patternChecking(String inputPattern,String validPattern)
+    {
+        if(inputPattern.matches(validPattern))
+        {
+            System.out.println("Valid pattern");
+        }
+        else
+        {
+            System.out.println("Not valid pattern");
+        }
+    }
     public static void main(String[] args)
     {
+        //variables
+        String firstName="";
         System.out.println("Welcome to user registration");
+        Scanner sc=new Scanner(System.in);
+        //Checking First name is valid or not
+        System.out.println("Enter first name:");
+        firstName=sc.next();
+        patternChecking(firstName,NAMEPATTERN);
     }
 }
